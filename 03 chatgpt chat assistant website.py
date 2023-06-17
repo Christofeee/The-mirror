5649e6695ea5eae110cb213dd4b78edd91b9963b
Christofeee <117144492+Christofeee@users.noreply.github.com>
2023-06-17 12:35:56 +0630

    update

--- 03 chatgpt chat assistant website.py
+++ 03 chatgpt chat assistant website.py
@@ -1,12 +1,12 @@
 import openai
 import gradio
 
-openai.api_key = "####"
+openai.api_key = "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"
 
-messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+messages = [{"role": "system", "content": "you are a person with mirror personality"}]
 
-def CustomChatGPT(user_input):
-    messages.append({"role": "user", "content": user_input})
+def CustomChatGPT(Talk_to_myself):
+    messages.append({"role": "user", "content": Talk_to_myself})
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = messages
@@ -15,6 +15,6 @@ def CustomChatGPT(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Hey! I'm You")
 
 demo.launch(share=True)
